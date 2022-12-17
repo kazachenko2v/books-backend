890fadc931ff905d2c00323d3aef53408b1aeb60
kazachenko2v <kazachenko2v@gmail.com>
2022-12-17 17:43:35 +0300

    commit 2

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
-import mysql from "mysql";
 import cors from "cors";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123qweasdzxc",
-  database: "test",
-});
 
 app.use(express.json());
 app.use(cors());
@@ -18,44 +16,18 @@ app.get("/", (req, res) => {
   res.json("hello");
 });
 
-app.get("/books", (req, res) => {
-  const q = "SELECT * FROM books";
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
-
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.cover];
-  db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("data");
-  });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const booksId = req.params.id;
-  const q = "DELETE FROM books WHERE id = ?";
-
-  db.query(q, [booksId], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("books deleted");
-  });
-});
-
-app.put("/books/:id", (req, res) => {
-  const booksId = req.params.id;
-  const q =
-    "UPDATE books SET `title` = ?, `desc` = ?, `cover` = ? WHERE id = ?";
-
-  const values = [req.body.title, req.body.desc, req.body.cover];
-
-  db.query(q, [...values, booksId], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("books updated");
-  });
+app.get("/documents1", (req, res) => {
+  fs.readFile(
+    path.resolve(__dirname, "db/documents1.json"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.json(JSON.parse(data));
+    }
+  );
 });
 
 app.listen(process.env.PORT || 8800, () => {
